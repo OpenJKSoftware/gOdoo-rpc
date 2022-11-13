8a99e3fb1f821f15c4f6877bc860cadf1f6e7aaa
Joshua Kreuder <joshua_kreuder@outlook.de>
2022-11-13 20:44:53 +0100

    [FIX] Version in __init__

--- wodoo_rpc/__init__.py
+++ wodoo_rpc/__init__.py
@@ -1,5 +1,5 @@
 """Wodoo-rpc."""
-__version__ = "0.3.4"
+__version__ = "0.0.2"
 from . import odoo2odoo_mapper  # noqa
 from .api import OdooApiWrapper  # noqa
 from .file_2_odoo import import_dataset, import_folder  # noqa
