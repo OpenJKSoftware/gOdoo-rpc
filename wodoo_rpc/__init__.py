f7381937cfe35d1cc72c6a906dbb6a14b7bfda25
Joshua <joshua_kreuder@outlook.de>
2022-11-17 17:39:47 +0000

    [BUMP] Package 0.0.5 to 0.0.6

--- wodoo_rpc/__init__.py
+++ wodoo_rpc/__init__.py
@@ -1,5 +1,5 @@
 """Wodoo-rpc."""
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 from . import odoo2odoo_mapper  # noqa
 from .api import OdooApiWrapper  # noqa
 from .file_2_odoo import import_data, import_dataset  # noqa
